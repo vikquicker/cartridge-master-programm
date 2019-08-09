d5fd59eab93dfa01e024898c7eebf8c29c759275
vikquicker <vik3_3stavrik3@mail.ru>
2019-08-09 21:08:24 +0300

    add iml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,16 +3,24 @@ package sample;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = new VBox();
+        VBox root = new VBox();
+        ScrollPane scrollPane1 = new ScrollPane();
+        ScrollPane scrollPane2 = new ScrollPane(new Text("log...."));
+
+        root.getChildren().addAll(scrollPane1, scrollPane2);
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1200, 700));
         primaryStage.show();
     }
 
