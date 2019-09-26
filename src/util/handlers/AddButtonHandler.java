e46660e31ad763ae8b563c7420008da7a76be9b0
vikquicker <vik3_3stavrik3@mail.ru>
2019-09-26 14:05:55 +0300

    add something

--- src/util/handlers/AddButtonHandler.java
+++ src/util/handlers/AddButtonHandler.java
@@ -3,7 +3,10 @@ package util.handlers;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -17,6 +20,20 @@ public class AddButtonHandler implements EventHandler<ActionEvent> {
 
         newWindow.initModality(Modality.APPLICATION_MODAL);
         newWindow.setResizable(false);
+
+        VBox vBox = new VBox();
+        HBox numberBox = new HBox();
+        HBox statusBox = new HBox();
+        HBox dataBox = new HBox();
+        HBox locationBox = new HBox();
+        HBox noticeBox = new HBox();
+
+        Label numberLabel = new Label("Номер");
+        Label statusLabel = new Label("Статус");
+        Label dataLabel = new Label("Дата");
+        Label locationLabel = new Label("Расположение");
+        Label noticeLabel = new Label("Примечание");
+
 
         newWindow.show();
     }
